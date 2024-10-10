206d1e9dfb1c30e098fca30000eaaad826e26e22
NathalyLima <127849560+NathalyLima@users.noreply.github.com>
2024-10-10 12:27:01 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Relogio from './Relogio';
+import Letreiro from './Letreiro';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <>
+  <Relogio/>
+  <Letreiro/>
+  </>
+);
+
+
