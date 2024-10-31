b6d8959689d959791ae015f86bf802e5297fde70
NathalyLima <127849560+NathalyLima@users.noreply.github.com>
2024-10-31 11:35:35 -0300

    Update Atividade2.js

--- src/Atividade2.js
+++ src/Atividade2.js
@@ -1,1 +1,17 @@
-
+import React from "react";
+import { Link } from "react-router-dom";
+import Relogio from "./Relogio"; 
+import Letreiro from "./Letreiro";
+ 
+function Atividade2() {
+    return (
+<>
+<h1>Atividade 02</h1>
+<Relogio />  
+<Letreiro />  
+<Link to="/">Voltar para a Home</Link> 
+</>
+    );
+}
+ 
+export default Atividade2;
