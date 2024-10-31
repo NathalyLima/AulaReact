ec25c70909267f396d2beb6e54a631bd51fa88e2
NathalyLima <127849560+NathalyLima@users.noreply.github.com>
2024-10-31 11:27:35 -0300

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,17 @@
 import React from "react";
-import {Link} from "react-router-dom";
-
-count Home = () => {
-  return(
-    <div>
-      <h1> Página Inicial</h1>
-      <nav>
-          <ul>
-              <li><Link to="/Atividade1"> Atividade 1</Link></li>
-          <ul>
-      </nav>
-    </div>
-  );
+import { Link } from "react-router-dom";
+ 
+const Home = () => {
+    return (
+<div>
+<h1>Página Inicial</h1>
+<nav>
+<ul>
+<li><Link to="/Atividade1">Atividade 1</Link></li>
+</ul>
+</nav>
+</div>
+    );
 }
-
+ 
 export default Home;
