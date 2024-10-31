d53007aae61833c823bd66dd2fc194dda4a1704e
NathalyLima <127849560+NathalyLima@users.noreply.github.com>
2024-10-31 11:56:57 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import MinhasRotas from './Minhasrotas';
+import Minhasrotas from './Minhasrotas';
  
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
