8aaa55cb18f2e07a91e5b0440867f87d3b24e09b
NathalyLima <127849560+NathalyLima@users.noreply.github.com>
2024-10-31 12:17:00 -0300

    Add files via upload

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+ 
+const Home = () => {
+    return (
+<div>
+<h1>Página Inicial</h1>
+<nav>
+<ul>
+<li><Link to="/Atividade1">Atividade 1</Link></li>
+</ul>
+</nav>
+</div>
+    );
+}
+ 
+export default Home;
